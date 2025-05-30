27a7f093c45f0b8640a7064fa73138948897b8b8
Umair Waheed <122041430+Umair-Waheed@users.noreply.github.com>
2025-05-30 11:44:37 +0500

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
     const seekBg=useRef();
     const seekBar=useRef();
 
-    const url='http://localhost:4000';
+    const url='https://sunny-tidal-archduke.glitch.me';
 
     const[songsData,setSongsData]=useState([]);
     const[albumsData,setAlbumsData]=useState([]);
